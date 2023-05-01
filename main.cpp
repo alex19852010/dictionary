b7511971e0d20797b2cb5523c1c008781e8e0285
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-01 18:34:30 +0300

    new status

--- main.cpp
+++ main.cpp
@@ -1,70 +1,39 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-map<string, string> directory;
-
+map <string, string> numbertosurname;
+map <string, vector <string>> surnametonumber;
 
 int main() {
-    while (1) {
-        string choice;
-        string number;
-        string surname;
-        cout << "enter number:";
-        cin >> number;
-        cout << "enter surname:";
-        cin >> surname;
-        directory.insert(pair<string, string>(number, surname));
 
-        cout << "if you want know the number click surname or if you want know the surname click number." << endl;
-        cout << "click  surname or number:";
-        cin >> choice;
+    string number;
+    string surname;
 
 
-        if (choice == "surname") {
-            cout << "enter surname:";
-            cin >> surname;
-            int count_1 = 0;
-            for (map<string, string>::iterator it = directory.begin(); it != directory.end(); it++) {
-                if (surname == it->second) {
-                    cout << it->first << " ";
-                    count_1++;
-                }
+    cout << "enter number:";
+    cin >> number;
+    cout << "enter surname:";
+    cin >> surname;
+    numbertosurname.insert(pair <string, string> (number, surname));
+    surnametonumber.insert(pair <string, vector <string>> (surname, vector <string>()));
+    surnametonumber[0].push_back(number);
 
-            }
-            if (!count_1) {
-                cout << "there is no such surname." << endl;
-                continue;
-            }
+    map <string, vector <string>> :: iterator it = surnametonumber.find(surname);
+    cout << it->second[0] << endl;
+    cout << endl;
 
-            cout << endl;
 
-        } else if (choice == "number") {
-            cout << "enter number:";
-            cin >> number;
-            int count_2 = 0;
-            for (map<string, string>::iterator it = directory.begin(); it != directory.end(); it++) {
-                if (number == it->first) {
-                    cout << it->second << endl;
-                    count_2++;
-                }
 
-            }
-            if (!count_2) {
-                cout << "there is no such number." << endl;
-                continue;
-            }
-        } else {
-            cout << "invalid information" << endl;
-            continue;
-        }
-      cout << "yes";
 
-      cout << "no";
 
-    }
     return 0;
 }
+
+
+
+
 
