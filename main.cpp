a96a20d476998bee2515b374710df817b23bfb85
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-03 12:48:21 +0300

    telephone

--- main.cpp
+++ main.cpp
@@ -5,63 +5,68 @@
 
 using namespace std;
 
-map <string, string> numbertosurname;
-map <string, vector <string>> surnametonumber;
+map<string, string> numbertosurname;
+map<string, vector<string>> surnametonumber;
 
 int main() {
 
     string number;
     string surname;
-    vector <string> name;
-    int count = 0;
-    int chooise = 0;
+    vector<string> name;
+    string choice;
 
-    while(1)
-    {
+    while (1) {
+        int count = 0;
         cout << "enter number:";
         cin >> number;
         cout << "enter surname:";
         cin >> surname;
-        numbertosurname.insert(pair <string, string> (number, surname));
-        surnametonumber.insert(pair <string, vector <string>> (surname, vector <string>()));
+        numbertosurname.insert(pair<string, string>(number, surname));
+        surnametonumber.insert(pair<string, vector<string>>(surname, vector<string>()));
 
 
-        for(map <string, string> :: iterator it = numbertosurname.begin(); it != numbertosurname.end(); it ++)
-        {
-            if (it->second == surname)
-            {
-                count ++;
+        for (map<string, string>::iterator it = numbertosurname.begin(); it != numbertosurname.end(); it++) {
+            if (it->second == surname) {
+                count++;
                 break;
             }
         }
 
 
-
-        if(count != 0)
-        {
+        if (count != 0) {
             surnametonumber[surname].push_back(number);
-        }
-
-        else
-        {
+        } else {
             surnametonumber[surname].push_back(number);
         }
 
 
+        cout << "if you want know the number click surname or if you want know the surname click number." << endl;
+        cout << "click  surname or number:";
+        cin >> choice;
 
+        if (choice == "number") {
+            cout << "enter number:";
+            cin >> number;
+            map<string, string>::iterator it = numbertosurname.find(number);
+            cout << it->second << endl;
 
+        } else if (choice == "surname") {
+            cout << "enter surname:";
+            cin >> surname;
 
-    cout << "enter surname:";
-    cin >> surname;
+            map<string, vector<string>>::iterator its = surnametonumber.find(surname);
+            for (int i = 0; i < its->second.size(); i++) {
+                cout << its->second[i] << " ";
+            }
+           cout << endl;
+        } else {
+            cout << "invalid information" << endl;
 
-    map <string, vector <string>> :: iterator its = surnametonumber.find(surname);
-    for(int i = 0; i < its->second.size(); i ++)
+        }
 
-    {
-        cout << its->second[i] << endl;
     }
 
-    }
+
     return 0;
 }
 
