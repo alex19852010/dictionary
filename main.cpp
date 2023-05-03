1d0fc6e93da2c2e26711f188b5593693d3450847
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-03 12:01:28 +0300

    gfd

--- main.cpp
+++ main.cpp
@@ -12,19 +12,59 @@ int main() {
 
     string number;
     string surname;
+    vector <string> name;
+    int count = 0;
+    int temp = 3;
+    int chooise = 0;
+
+    while(temp != 0)
+    {
+        cout << "enter number:";
+        cin >> number;
+        cout << "enter surname:";
+        cin >> surname;
+        numbertosurname.insert(pair <string, string> (number, surname));
+        surnametonumber.insert(pair <string, vector <string>> (surname, vector <string>()));
 
 
-    cout << "enter number:";
-    cin >> number;
-    cout << "enter surname:";
+        for(map <string, string> :: iterator it = numbertosurname.begin(); it != numbertosurname.end(); it ++)
+        {
+            if (it->second == surname)
+            {
+                count ++;
+                break;
+            }
+        }
+
+
+
+        if(count != 0)
+        {
+            surnametonumber[surname].push_back(number);
+        }
+
+        else
+        {
+            surnametonumber[surname].push_back(number);
+        }
+        temp --;
+
+        cout << "if you want find out the number,then press 1,if you want find out the last name,then press 2,if you want enter data, then press 3" << endl;
+        
+    }
+
+    cout << "rnter surname:";
     cin >> surname;
-    numbertosurname.insert(pair <string, string> (number, surname));
-    surnametonumber.insert(pair <string, vector <string>> (surname, vector <string>()));
-    surnametonumber[0].push_back(number);
 
-    map <string, vector <string>> :: iterator it = surnametonumber.find(surname);
-    cout << it->second[0] << endl;
-    cout << endl;
+    map <string, vector <string>> :: iterator its = surnametonumber.find(surname);
+    for(int i = 0; i < its->second.size(); i ++)
+
+    {
+        cout << its->second[i] << endl;
+    }
+
+
+
 
 
 
