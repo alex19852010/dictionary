e954142f2e114d44cca3a888d5ef6398b486ada0
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-03 12:56:20 +0300

    telephone2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,61 @@ map<string, string> numbertosurname;
 map<string, vector<string>> surnametonumber;
 
 int main() {
+    string number;
+    string surname;
+    vector<string> name;
+    string choice;
+
+    while (1) {
+        int count = 0;
+        cout << "enter number:";
+        cin >> number;
+        cout << "enter surname:";
+        cin >> surname;
+        numbertosurname.insert(pair<string, string>(number, surname));
+        surnametonumber.insert(pair<string, vector<string>>(surname, vector<string>()));
+
+
+        for (map<string, string>::iterator it = numbertosurname.begin(); it != numbertosurname.end(); it++) {
+            if (it->second == surname) {
+                count++;
+                break;
+            }
+        }
+
+
+        if (count != 0) {
+            surnametonumber[surname].push_back(number);
+        } else {
+            surnametonumber[surname].push_back(number);
+        }
+
+
+        cout << "if you want know the number click surname or if you want know the surname click number." << endl;
+        cout << "click  surname or number:";
+        cin >> choice;
+
+        if (choice == "number") {
+            cout << "enter number:";
+            cin >> number;
+            map<string, string>::iterator it = numbertosurname.find(number);
+            cout << it->second << endl;
+
+        } else if (choice == "surname") {
+            cout << "enter surname:";
+            cin >> surname;
+
+            map<string, vector<string>>::iterator its = surnametonumber.find(surname);
+            for (int i = 0; i < its->second.size(); i++) {
+                cout << its->second[i] << " ";
+            }
+            cout << endl;
+        } else {
+            cout << "invalid information" << endl;
+
+        }
+
+    }
 
     return 0;
 }
