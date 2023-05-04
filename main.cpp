86662b132b685597c39a375f047f723f9aa0112c
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-04 16:37:12 +0300

    telephone6

--- main.cpp
+++ main.cpp
@@ -30,7 +30,30 @@ int main() {
  else
      surnametonumber.insert(pair<string, vector<string>>(surname, vector<string>()));
 
+        cout << "if you want know the number click surname or if you want know the surname click number." << endl;
+        cout << "click  surname or number:";
+        cin >> choice;
 
+        if (choice == "number") {
+            cout << "enter number:";
+            cin >> number;
+            map<string, string>::iterator it = numbertosurname.find(number);
+            cout << it->second << endl;
+
+        } else if (choice == "surname") {
+            cout << "enter surname:";
+            cin >> surname;
+
+            map<string, vector<string>>::iterator its = surnametonumber.find(surname);
+            for (int i = 0; i < its->second.size(); i++) {
+                cout << its->second[i] << " ";
+            }
+
+            cout << endl;
+        } else {
+            cout << "invalid information" << endl;
+
+        }
 
 
 }
