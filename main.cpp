e1672d202424c3a97b1a134ad40e254970c9d79d
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-05 17:57:41 +0300

    30phone

--- main.cpp
+++ main.cpp
@@ -13,39 +13,65 @@ string surname;
 string choice;
 
 int main() {
-    while(1)
-    {
+    while (1) {
         while (choice != "number" && choice != "surname") {
             cout << "enter number:";
             cin >> number;
             cout << "enter surname:";
             cin >> surname;
 
-            numbertosurname.insert(pair<string, string> (number,surname));
+            numbertosurname.insert(pair<string, string>(number, surname));
+            auto its = surnametonumber.find(surname);
+            if (its != surnametonumber.end()) {
+                surnametonumber[surname].push_back(number);
+            } else {
+                surnametonumber.insert(pair<string, vector<string>>(surname, vector<string>()));
+                surnametonumber[surname].push_back(number);
+            }
+
             cout << "if you want to know number, enter number, if you want to know surname, enter surname:" << endl;
             cout << "make a choice: ";
             cin >> choice;
 
         }
 
-        if(choice == "surname")
+        if (choice == "surname") {
             cout << "enter number:";
-        cin >> number;
-        auto it = numbertosurname.find(number);
-        if(it != numbertosurname.end())
-        {
-            cout << it->second << endl;
-            choice = "";
-            continue;
-        }
-        else
-        {
-            cout << "invalid number" << endl;
-            choice = "";
-            continue;
+            cin >> number;
+            auto it = numbertosurname.find(number);
+
+            if (it != numbertosurname.end()) {
+                cout << it->second << endl;
+                choice = "";
+                continue;
+            } else {
+                cout << "invalid number" << endl;
+                choice = "";
+                continue;
+            }
+
         }
 
+        if (choice == "number") {
+            cout << "enter surname:";
+            cin >> surname;
+            auto m = surnametonumber.find(surname);
 
+            if (m != surnametonumber.end()) {
+                for (int i = 0; i < m->second.size(); i++) {
+                    cout << m->second[i] << " ";
+                }
+                cout << endl;
+                choice = "";
+                continue;
+            } else {
+                cout << "invalid surname" << endl;
+                choice = "";
+                continue;
+            }
+
+
+        }
 
     }
 
